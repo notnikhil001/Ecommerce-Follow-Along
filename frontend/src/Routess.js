8c45d2bc9eeb264dc4c54575009baa9244b58d5b
Nikhil Farand <nikhilfarand@Nikhils-MacBook-Air-4.local>
2025-02-06 14:41:05 +0530

    milestone5

--- frontend/src/Routess.js
+++ frontend/src/Routess.js
@@ -1,7 +1,10 @@
+
+
 import LoginPage from "../src/pages/Login.jsx"
+import SignUpPage from "../src/pages/SignUpPage.jsx"
 
 export
 {
-    LoginPage
+    LoginPage,
+    SignUpPage
 }
-
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Signup from "../components/SignUp/SignUp.jsx"
+const SignUpPage = () => {
+  return (
+    <div>
+        <Signup/>
+    </div>
+  )
+}
+
+export default SignUpPage
