653d337ed0581495a30ff5960efb7397d9878796
Nikhil Farand <nikhilfarand@Nikhils-MacBook-Air-4.local>
2025-02-25 09:44:15 +0530

    assignment

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import {LoginPage,  Home, ActivationPage, SignUpPage,CreateProduct,MyProducts} from "./Routess.js";
+import "./App.css";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<LoginPage/>}/>
+      <Route path="/sign-up" element={<SignUpPage/>} />
+      <Route path="/activation/:activation_token" element={<ActivationPage />} />
+      <Route path='/create-product' element={<CreateProduct/>}/>
+      <Route path="/create-product/:id" element={<CreateProduct />} />
+      <Route path="/my-products" element={<MyProducts/>} />
+    </Routes>
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
