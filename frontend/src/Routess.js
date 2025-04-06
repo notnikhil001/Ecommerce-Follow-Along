b6ea070388eda1785397f110bbf7e329b469b4c8
Nikhil Farand <nikhilfarand@Nikhils-MacBook-Air-4.local>
2025-04-06 22:43:17 +0530

    milestone27

---
+++ frontend/src/Routess.js
@@ -0,0 +1,30 @@
+import LoginPage from "../src/pages/Login.jsx"
+import SignUpPage from "../src/pages/SignUpPage.jsx"
+import ActivationPage from "../src/pages/ActivationPage.jsx";
+import Home from "../src/pages/Home.jsx"
+import CreateProduct from "../src/pages/createProduct.jsx"
+import MyProducts from "../src/pages/myProducts.jsx";
+import Cart from "./pages/cart.jsx";
+import ProductDetails from "./pages/productDetails.jsx";
+import Profile from "./pages/profile.jsx";
+import CreateAddress from "./pages/createAddress.jsx";
+import SelectAddress from "./pages/SelectAddress.jsx";
+import OrderConfirmation from "./pages/OrderConfirmation.jsx";
+import MyOrdersPage from "./pages/myorders.jsx";
+export
+{
+    LoginPage,
+    SignUpPage,
+    ActivationPage,
+    Home,
+    CreateProduct,
+    MyProducts,
+    Cart,
+    ProductDetails,
+    Profile,
+    CreateAddress,
+    SelectAddress,
+    OrderConfirmation,
+    MyOrdersPage
+
+}
